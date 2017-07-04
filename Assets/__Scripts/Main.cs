bfe4442d484ef4479746b2be7fd31990f76c6c78
Richie Nguyen <richieytl@gmail.com>
2017-07-03 20:42:09 -0400

    Updated the scene. Created enemy ships. Deleted empty Utils script. Created basic Enemy script. Started Main script.

---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	static public Main S;
+
+	public GameObject[] prefabEnemies;
+	public float enemySpawnPerSecond = .5f;
+	public float enemySpawnPadding = 1.5f;
+
+	public float enemySpawnRate;
+
+	void Awake(){
+		S = this;
+		Utils.SetCameraBounds (this.camera);
+		enemySpawnRate = 1f / enemySpawnPerSecond;
+		Invoke ("SpawnEnemy", enemySpawnRate);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
