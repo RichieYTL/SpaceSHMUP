6111f7af8ed5492f1f35e34fe91588c31cf971e1
Richie Nguyen <richieytl@gmail.com>
2017-07-11 18:45:32 -0400

    Updated main script to include enemy spawn. Changed scene's size of enemy selection to 1, with enemy_0 as the only enemy in the array.

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -14,18 +14,20 @@ public class Main : MonoBehaviour {
 
 	void Awake(){
 		S = this;
-		Utils.SetCameraBounds (this.camera);
+		Utils.SetCameraBounds (this.GetComponent<Camera>());
 		enemySpawnRate = 1f / enemySpawnPerSecond;
 		Invoke ("SpawnEnemy", enemySpawnRate);
 	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void SpawnEnemy(){
+		int ndx = Random.Range (0, prefabEnemies.Length);
+		GameObject go = Instantiate (prefabEnemies [ndx]) as GameObject;
+		Vector3 pos = Vector3.zero;
+		float xMin = Utils.camBounds.min.x + enemySpawnPadding;
+		float xMax = Utils.camBounds.max.x - enemySpawnPadding;
+		pos.x = Random.Range (xMin, xMax);
+		pos.y = Utils.camBounds.max.y + enemySpawnPadding;
+		go.transform.position = pos;
+		Invoke ("SpawnEnemy", enemySpawnRate);
 	}
 }
